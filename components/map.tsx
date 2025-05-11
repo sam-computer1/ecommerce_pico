0a244e153e7acade5a6b7124f77feae0406a4511
Samarpan <samarpansmacbookpro@Samarpans-MacBook-Pro.local>
2025-05-11 23:03:00 +0530

    updated contact page and FAQ page

--- components/map.tsx
+++ components/map.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useEffect, useState } from 'react'
+
 interface Location {
   lat: number
   lng: number
@@ -13,15 +15,21 @@ interface MapProps {
 }
 
 export default function Map({ className = "", location }: MapProps) {
-  // Generate Google Maps embed URL - now using the same URL for all locations
-  const getMapUrl = () => {
-    // New map URL for PICo. location
-    return "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7839.16529965385!2d106.652492!3d10.766613!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fa5e3e68613%3A0x47bc8460635e0f28!2sPICo.!5e0!3m2!1sen!2sin!4v1746437167571!5m2!1sen!2sin"
-  };
+  const [mapUrl, setMapUrl] = useState<string>('')
+  
+  useEffect(() => {
+    // Set map URL only on client side
+    setMapUrl("https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7839.16529965385!2d106.652492!3d10.766613!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fa5e3e68613%3A0x47bc8460635e0f28!2sPICo.!5e0!3m2!1sen!2sin!4v1746437167571!5m2!1sen!2sin")
+  }, [])
+
+  // Don't render until URL is available on client
+  if (!mapUrl) {
+    return <div className={`w-full h-full bg-gray-100 flex items-center justify-center ${className}`}>Loading map...</div>
+  }
 
   return (
     <iframe 
-      src={getMapUrl()}
+      src={mapUrl}
       className={`w-full h-full ${className}`}
       style={{ border: 0 }}
       allowFullScreen
